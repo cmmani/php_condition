f8b3219054b844140fc61ff14131522a07999a57
cmmani <muthudevopsengineer@gmail.com>
2023-05-29 11:46:01 +0530

    this is my sample php program

---
+++ if.php
@@ -0,0 +1,21 @@
+//while<br>
+
+<?php
+$n=-1;
+while($n<=3){
+echo "$n<br/>";
+$n++;
+}
+?>
+
+
+<br><br>
+
+//do while<br>
+<?php
+$n=-2;
+do{
+echo "$n<br/>";
+$n++;
+}while($n<=5);
+?>
---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+$num="audi";
+switch($num){
+case "audi":
+echo("luxurious car ");
+break;
+case "benz":
+echo("its branded one");
+break;
+case "porsche":
+echo("sports car");
+break;
+default:
+echo("its a car ");
+}
+?>
